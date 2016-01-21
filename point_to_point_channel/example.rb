bbd308e35cf53a474138b9554187d2d076fcf31e
James Edward Gray II <james@graysoftinc.com>
2016-01-21 17:09:25 -0600

    Point to point example.

---
+++ point_to_point_channel/example.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+require "aws-sdk"
+
+require_relative "../shared/aws_config"
+
+sqs = Aws::SQS::Client.new
+queue = sqs.create_queue(queue_name: "point_to_point_channel")
+
+pids = Array.new(3) {
+  fork do
+    puts "Child process #{Process.pid} waiting for messages...\n"
+    poller = Aws::SQS::QueuePoller.new(queue[:queue_url])
+    poller.poll do |msg|
+      puts "Processing '#{msg.body}' in #{Process.pid}.\n"
+    end
+  end
+}
+at_exit do
+  pids.each do |pid|
+    Process.kill("TERM", pid)
+  end
+end
+
+10.times do |i|
+  puts "Pushing message #{i}..."
+  sqs.send_message(queue_url: queue[:queue_url], message_body: "Message #{i}")
+end
+
+sleep 3  # allow for processing
