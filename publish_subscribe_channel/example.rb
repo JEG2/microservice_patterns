f74c6e5078e70fc378741c7e6663c7b7abc59f7d
James Edward Gray II <james@graysoftinc.com>
2016-01-24 15:05:34 -0600

    First working version of pub-sub.

--- publish_subscribe_channel/example.rb
+++ publish_subscribe_channel/example.rb
@@ -7,67 +7,63 @@ require "json"
 
 require_relative "../shared/aws_config"
 
-sqs = Aws::SQS::Client.new
-queue = sqs.create_queue(queue_name: "publish_subscribe_channel_0")
-queue_attributes = sqs.get_queue_attributes(
-  queue_url: queue.queue_url,
-  attribute_names: ["QueueArn"]
-)
-
 sns = Aws::SNS::Client.new
+sqs = Aws::SQS::Client.new
 topic = sns.create_topic(name: "publish_subscribe_channel")
-subscription = sns.subscribe(
-  topic_arn: topic.topic_arn,
-  protocol: "sqs",
-  endpoint: queue_attributes.attributes["QueueArn"]
-)
 
-sqs.set_queue_attributes(
-  queue_url: queue.queue_url,
-  attributes: {
-    "Policy" => {
-      "Version" => "2012-10-17",
-      "Statement" => [
-        {
-          "Effect" => "Allow",
-          "Principal" => "*",
-          "Action" => "sqs:SendMessage",
-          "Resource" => queue_attributes.attributes["QueueArn"],
-          "Condition" => {
-            "ArnEquals" => {
-              "aws:SourceArn" => topic.topic_arn
+queue_urls = Array.new(3) { |i|
+  queue = sqs.create_queue(queue_name: "publish_subscribe_channel_#{i}")
+  queue_attributes = sqs.get_queue_attributes(
+    queue_url: queue.queue_url,
+    attribute_names: ["QueueArn"]
+  )
+  subscription = sns.subscribe(
+    topic_arn: topic.topic_arn,
+    protocol: "sqs",
+    endpoint: queue_attributes.attributes["QueueArn"]
+  )
+  sqs.set_queue_attributes(
+    queue_url: queue.queue_url,
+    attributes: {
+      "Policy" => {
+        "Version" => "2012-10-17",
+        "Statement" => [
+          {
+            "Effect" => "Allow",
+            "Principal" => "*",
+            "Action" => "sqs:SendMessage",
+            "Resource" => queue_attributes.attributes["QueueArn"],
+            "Condition" => {
+              "ArnEquals" => {
+                "aws:SourceArn" => topic.topic_arn
+              }
             }
           }
-        }
-      ]
-    }.to_json
-  }
-)
+        ]
+      }.to_json
+    }
+  )
+  queue.queue_url
+}
 
-# resp = sqs.receive_message(
-#   queue_url: queue.queue_url,
-#   wait_time_seconds: 20
-# )
-# p resp
+pids = Array.new(queue_urls.size) { |i|
+  fork do
+    puts "Child process #{Process.pid} waiting for messages...\n"
+    poller = Aws::SQS::QueuePoller.new(queue_urls[i])
+    poller.poll do |msg|
+      puts "Processing '#{msg.body}' in #{Process.pid}.\n"
+    end
+  end
+}
+at_exit do
+  pids.each do |pid|
+    Process.kill("TERM", pid)
+  end
+end
 
-# pids = Array.new(3) {
-#   fork do
-#     puts "Child process #{Process.pid} waiting for messages...\n"
-#     poller = Aws::SQS::QueuePoller.new(queue[:queue_url])
-#     poller.poll do |msg|
-#       puts "Processing '#{msg.body}' in #{Process.pid}.\n"
-#     end
-#   end
-# }
-# at_exit do
-#   pids.each do |pid|
-#     Process.kill("TERM", pid)
-#   end
-# end
+3.times do |i|
+  puts "Pushing message #{i}..."
+  sns.publish(topic_arn: topic.topic_arn, message: "Message #{i}")
+end
 
-# 10.times do |i|
-#   puts "Pushing message #{i}..."
-#   sqs.send_message(queue_url: queue[:queue_url], message_body: "Message #{i}")
-# end
-
-# sleep 3  # allow for processing
+sleep 3  # allow for processing
